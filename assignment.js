a07728b306b8d65433cbde54b9cf6091435193f1
PranjaliNandkumarUikey <pranjaliuikey22@navgurukul.org>
2024-03-25 00:07:03 +0530

    Javascript Assignment

---
+++ assignment.js
@@ -0,0 +1,101 @@
+/*1.Take two numbers a,b and print the sum, difference, product, quotient and modulus of a & b.*/
+// let a=975;
+// let b=85;
+// let Sum = a + b; 
+// let Diff = a - b;
+// let Prod = a * b;
+// let Quo = a / b;
+// let Mod = a % b;
+// console.log(Sum,Diff,Prod,Quo,Mod);
+
+/*2. Take two numbers and only print the integer part when a is divided by b.*/
+// let a=10;
+// let b=5;
+// let C = a%b;
+// console.log(C);
+
+/*3. Take two numbers a,b and print the exponential power of the first number raised by the second.*/
+// let a = 10;
+// let b = 5;
+// let E = a**b;
+// console.log(E);
+
+/*4. Take two numbers a,b and only print the decimal part of the result obtained when a is divided by b. */
+// let a = 100;
+// let b = 54;
+// let D = a / b;
+// console.log(D);
+
+/*5. Take marks in 5 subjects A,B,C,D,E and print out the average of marks from five subject marks.*/
+
+// let a = 89;
+// let b = 95;
+// let c = 94;
+// let d = 83;
+// let e = 77;
+// let S = a + b + c + d + e;
+// let A = S/5; // Average
+// console.log(A);
+
+/*6. Take the cost price a and selling price b of a product, and print the profit obtained on the product.*/
+
+// let a = 10 ;//Selling Price
+// let b = 30; // Cost Price
+// let P = a - b ;// Profit = S.P - C.P
+// console.log(P);
+
+/*7. Take a number and print out the last digit of the number.*/ 
+// let a = 245;
+// let S = a % 10;
+// console.log(S);
+
+/*8. Take a 2 digit number and return the first and last digits.*/ 
+// let a = 45;
+// let F = a / 10;
+// let L = a % 10;
+// console.log(F);
+// console.log(L);
+
+/*9. Write a JavaScript program to find the largest of three numbers.*/
+
+// let a = 67;
+// let b = 80;
+// let c = 54;
+
+// if(a>=b && a>=c){
+//     console.log(a);
+// }
+// else if(b>=a && b>=c){
+//     console.log(b);
+// }
+// else{
+//     console.log(c);
+// }
+
+/*10. Write a JavaScript program to check whether a number is even or odd.*/
+
+// let a = 50;
+// if(a%50==0) {
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd")
+// }
+
+/*11. Write a JavaScript program to check whether a given number is prime or not.*/
+
+// let a = 31; //number
+// let c = 0; // count
+
+// for(let i=1; i<=a; i++){
+//     if(a % i == 0){
+//         c++;
+//     }
+// }
+
+// if(c == 2){
+//     console.log("Prime");
+// }
+// else{
+//     console.log("Not Prime")
+// }
